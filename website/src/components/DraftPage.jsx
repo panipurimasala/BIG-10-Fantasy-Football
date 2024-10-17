8e7d1d37bf7da71e1c65adf547660a9c3437f0cf
msoud2509 <msouders06@gmail.com>
2024-10-16 20:53:38 -0500

    update draftpage.jsx

--- website/src/components/DraftPage.jsx
+++ website/src/components/DraftPage.jsx
@@ -2,29 +2,50 @@ import React from "react";
 
 const DraftPage = () => {
     return (
-<div className="market_table">
-        <table border="1">
-            <tr>
-                <th>QB</th>
-                <th>WR</th>
-                <th>O-LINE</th>
-                <th>TE</th>
-                <th>RB</th>
-                <th>SAFETY</th>
-                <th>D-LINE</th>
-                <th>LB</th>
-                <th>CB</th>
-                <th>SPECIAL TEAM</th>
-            </tr>
-            <tr>
-            </tr>
-            <tr>
-            </tr>
-            <tr>
-            </tr>
-            <tr>
-            </tr>
-        </table>
-</div>)
+<div className="Draft">
+    <div className="Home">
+            <form action="" method="get">
+                <input id="userquery" name="query" type="text" placeholder="Player Name"/>
+                <select name="category"> {/* should this be static or dynamic */}
+                    <option value="QB">QB</option>
+                    <option value="WR">WR</option>
+                    <option value="OLINE">O-LINE</option>
+                    <option value="TE">TE</option>
+                    <option value="RB">RB</option>\
+                    <option value="SFT">SAFETY</option>
+                    <option value="WR">D-LINE</option>
+                    <option value="LB">LB</option>
+                    <option value="CB">CB</option>
+                    <option value="SPT">SPECIAL</option>
+                </select>
+                <input type="submit"/>
+            </form>
+        </div>
+    <div className="market_table">
+            <table border="1">
+                <tr>
+                    <th>QB</th>
+                    <th>WR</th>
+                    <th>O-LINE</th>
+                    <th>TE</th>
+                    <th>RB</th>
+                    <th>SAFETY</th>
+                    <th>D-LINE</th>
+                    <th>LB</th>
+                    <th>CB</th>
+                    <th>SPECIAL TEAM</th>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                </tr>
+            </table>
+    </div>
+</div>
+)
 }
 export default DraftPage;
