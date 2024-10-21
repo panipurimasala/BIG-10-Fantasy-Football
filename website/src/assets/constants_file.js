c1a9729ea19642621a85053ebfaf22dff064251e
panipurimasala <57164495+panipurimasala@users.noreply.github.com>
2024-10-21 07:47:16 -0500

    new changes to homepage

---
+++ website/src/assets/constants_file.js
@@ -0,0 +1,23 @@
+import Braxton from "./Braxton-Berrios.png"
+import kurtis from "./Kurtis_Rourke.webp"
+const injuriesData = [
+  {
+    playerImage: Braxton,
+    name: "Braxton Berrios",
+    team: "Mia",
+    position: "WR",
+    status: "Q",
+    injury: "Knee"
+  },
+  {
+    playerImage: kurtis,
+    name: "Kurtis Rourke",
+    team: "Ind",
+    position: "QB",
+    status: "NA",
+    injury: "Undisclosed"
+  },
+]
+
+
+export default injuriesData;
---
+++ website/src/components/TopPlayerCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TopPlayerCard = ({pImage, pName, fpoints}) =>{
+    return (
+        <div className="PlayerC">
+
+        </div>
+    )
+}
