984f3d2930467f3d0ec4d2638007e86f659bb276
Nathanblaster12 <vaz00004@umn.edu>
2024-10-22 02:49:08 -0500

    imrproved team drafting

--- website/src/components/DraftPage.jsx
+++ website/src/components/DraftPage.jsx
@@ -12,9 +12,12 @@ function getRosterData(){
     return playerDict;
 }
 
-function draftPlayer(playerDict, player, team){
+function draftPlayer(playerDict, player, team, position){
     if(!Object.keys(playerDict).includes(player)){
         return "no player found";
+    }
+    if (playerDict[player].position !== position) {
+        return "player does not play this position"
     }
     let playerToAdd = playerDict[player];
     playerToAdd["name"] = player;
@@ -28,7 +31,7 @@ function testPrintPlayers(team){
     let retarr = [];
     Object.keys(team).forEach(pos => {
         team[pos].forEach(player => {
-            retarr.push(player.name + " " + player.position + ": ");
+            retarr.push(player.position + ": " + player.name + " ");
         })
     })
     return retarr;
@@ -63,36 +66,43 @@ let team2 = {"QB":[], "RB":[], "WR":[], "TE":[], "K":[], "D/ST":[]};
 const DraftPage = () => {
     const [inputText, setInputText] = useState('');
     const [filteredPlayers, setFilteredPlayers] = useState([]);
+    const [selectedPosition, setSelectedPosition] = useState("QB");
     const [displayText, setDisplayText] = useState("Start Drafting! Team 1 pick");
 
-    const handleInputChange = (e) => {
+    const handleInputChange = (e) => { // filter a list of 6 players based on selected position and names
         const text = e.target.value;
         setInputText(text);
         const match =  Object.keys(playerDict).some(player => player.toLowerCase() === text.toLowerCase());// hide the dropdown if whole name is typed in
         if (!match && text !== '') {
-        const filtered = Object.keys(playerDict).filter(player => 
+        const filtered = Object.keys(playerDict).filter(player => playerDict[player].position === selectedPosition && 
             player.toLowerCase().includes(text.toLowerCase())).slice(0, 6);
         setFilteredPlayers(filtered); 
     } else {
         setFilteredPlayers([]);
     }};
+    const handlePositionChange = (e) => { // change positions
+        setSelectedPosition(e.target.value);
+        setFilteredPlayers([]);
+    };
 
     // Create a function to handle the submit
     const handleSubmit = (e) => {
         e.preventDefault(); // Prevent the default form submission
         if (count % 2 === 0){
-            let ret = draftPlayer(playerDict, inputText, team1, count);
+            let ret = draftPlayer(playerDict, inputText, team1, selectedPosition);
             setDisplayText(ret); 
-            if(ret !== "no player found"){
+            if(ret !== "no player found" && ret !== "player does not play this position"){
                 count += 1;
             }
+            setDisplayText("Team 2 pick!");
         }
         else{
-            let ret = draftPlayer(playerDict, inputText, team2, count);
+            let ret = draftPlayer(playerDict, inputText, team2, selectedPosition);
             setDisplayText(ret);
-            if(ret !== "no player found"){
+            if(ret !== "no player found" && ret !== "player does not play this position"){
                 count += 1;
             }
+            setDisplayText("Team 1 pick!");
 
         }
         if (count === 4) {
@@ -117,9 +127,9 @@ const DraftPage = () => {
 
                     
                     {filteredPlayers.length > 0 && (
-                        <ul style={{ border: '0.5px solid', listStyle: 'Arial', padding: 20}}>
+                        <ul style={{ border: '0.5px solid', listStyle: 'Arial',  padding: 20}}>
                             {filteredPlayers.map((player, index) => (
-                                <li key={index} style={{ padding: '1px', cursor: 'wait' }}
+                                <li key={index} style={{ padding: '1px', cursor: 'grab', fontWeight: 'bold'}}
                                     onClick={() => {
                                         setInputText(player); 
                                         setFilteredPlayers([]); 
@@ -129,7 +139,7 @@ const DraftPage = () => {
                             ))}
                         </ul>
                     )}
-                <select name="category"> {/* should this be static or dynamic */}
+                <select name="category" onChange={handlePositionChange}> {/* should this be static or dynamic */}
                     <option value="QB">QB</option>
                     <option value="WR">WR</option>
                     <option value="OLINE">O-LINE</option>
