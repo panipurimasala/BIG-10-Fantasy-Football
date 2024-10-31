0481694b42fb7684ff57434f1a2a00f538598e5d
msoud2509 <msouders06@gmail.com>
2024-10-31 12:39:19 -0500

    add style to league page

--- website/src/components/LeaguePage.jsx
+++ website/src/components/LeaguePage.jsx
@@ -16,6 +16,8 @@ let tournamentDict = getTournamentData();
 function League() {
     const [tourneyName, setTourneyName] = useState ('');
     const [password, setPassword] = useState('');
+    const [createtourneyName, setCreateTourneyName] = useState ('');
+    const [createPassword, setCreatePassword] = useState('');
     const { isAuthenticated } = useAuth0();
     /* const handlePublicSubmit = (e) => { // for now only can join by used ID and password
         e.preventDefault();
@@ -42,6 +44,12 @@ function League() {
             setPassword('');
         }
     };
+    const handleCreatePrivateSubmit = (e) => {
+        e.preventDefault();
+        if (createtourneyName && createPassword) { // tourneyName means the ID in this function
+            
+        }
+    };
     /* const handlePrivateSubmit = (e) => {
         e.preventDefault();
         if (tourneyName && password) {
@@ -51,33 +59,83 @@ function League() {
             alert (" Invalid input");
         }
     }; */
+    const Divider = () => {
+        return (
+          <div className="divider-container">
+            <div className="divider-line" />
+            <span className="divider-text">- or -</span>
+            <div className="divider-line" />
+          </div>
+        );
+    };
+
 
     if (isAuthenticated) {
-        return (<div className="headerLogin">
-            <h1>Join a Private League</h1>
-            <form onSubmit={handlejoinPrivateSubmit}>
-                <label>
-                    <p className='passwordText'>League ID:</p>
-                    <input
-                        type="text"
-                        value={tourneyName}
-                        onChange={(e) => setTourneyName(e.target.value)}
-                        required
-                    />
-                </label>
-                <br />
-                <label>
-                    <p className='passwordText'>Password:</p>
-                    <input
-                        type="text"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <br />
-                <button className='leagueButton' type="submit">Join Private League</button>
-            </form>
+        return (
+        <div className='pageContainer'>
+            <div className='leagueContainer'>
+                <div className="headerLogin">
+                    <h1>Join a League</h1>
+                    <form onSubmit={handlejoinPrivateSubmit}>
+                        <label>
+                            <p className='passwordText'>League ID:</p>
+                            <input
+                                type="text"
+                                value={tourneyName}
+                                onChange={(e) => setTourneyName(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <br />
+                        <label>
+                            <p className='passwordText'>Password:</p>
+                            <input
+                                type="text"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <br />
+                        <button className='leagueButton' type="submit">Join League</button>
+                    </form>
+                </div>
+                <Divider />
+                <div className='createLeague'>
+                <h1>Create a League</h1>
+                    <form onSubmit={handleCreatePrivateSubmit}>
+                        <label>
+                            <p className='passwordText'>League ID:</p>
+                            <input
+                                type="text"
+                                value={createtourneyName}
+                                onChange={(e) => setCreateTourneyName(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <br />
+                        <label>
+                            <p className='passwordText'>Password:</p>
+                            <input
+                                type="text"
+                                value={createPassword}
+                                onChange={(e) => setCreatePassword(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <br />
+                        <button className='leagueButton' type="submit">Join League</button>
+                    </form>
+                </div>
+            </div>
+            <div className='currentLeaguesContainer'>
+                    <div className='currentHeader'><h1 className='currentLeaguesText'>Current Leagues</h1></div>
+                    <div className='currentLeaguesDisplay'>
+                        <h1>You are not in any Leagues</h1>
+                        <br />
+                        <h1>Join or Create One to Get Started!</h1>
+                    </div>
+            </div>
         </div>);
     }
     else {
